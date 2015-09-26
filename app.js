4697d526128b59db26a5b34a5ead43989413d25b
Daniel <dan_pc@live.com>
2015-09-25 22:50:39 -0400

    Setup dependencies

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/payment.html");
-});
-
-app.use(express.static(__dirname + "/public"));
-app.listen(3000);
