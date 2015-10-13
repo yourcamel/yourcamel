e19b53de84364488dfca6727dd3d37254bddf0f7
Daniel <dan_pc@live.com>
2015-10-12 23:55:55 -0400

    Testing ssl

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var customer = require('./customer.js');
 var app = express();
+var fs = require('fs');
+var https = require('https');
+
+var server = https.createServer({
+    key: fs.readFileSync('ssl/www_yourcamel_com.key'),
+    certificate: fs.readFileSync('ssl/yourcamel.crt'),
+    ca: [fs.readFileSync('ssl/gdb1.crt'), fs.readFileSync('ssl/gdb2.crt'), fs.readFileSync('ssl/gdb3.crt')]
+}, app);
+
+
+server.listen(process.env.PORT || 443);
 
 app.set('view engine', 'ejs');
 
@@ -12,7 +23,7 @@ app.use(bodyParser.json());
 
 app.post('/payment.html', function(req, res) {
 	customer.create(req.body.stripeToken, function() {
-		res.redirect("https://yourcamel.herokuapp.com");
+		res.redirect("https://yourcamel.com");
 	});
 });
 
@@ -20,5 +31,3 @@ app.use(function(req, res, next){
     res.status(404);
     res.render('404', { url: req.url });
 });
-
-app.listen(process.env.PORT || 8080);
